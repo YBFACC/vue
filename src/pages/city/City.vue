c0a317f0ecc28847e2b12fe015938575a31f0722
bingfeng yu <ybf0711@gmail.com>
2019-11-20 17:12:04 +0800

    scrollToElement

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,11 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 <script>
@@ -24,7 +27,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   mounted () {
@@ -33,6 +37,9 @@ export default {
   methods: {
     getCityInfo () {
       axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     },
     handleGetCityInfoSucc (res) {
       res = res.data
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,7 +1,16 @@
 <template lang="">
   <ul class="list">
-    <li class="item" v-for="(item, index) in cities" :key="index">
-      {{ index }}
+    <li
+      class="item"
+      v-for="item in letters"
+      :key="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      :ref="item"
+    >
+      {{ item }}
     </li>
   </ul>
 </template>
@@ -11,6 +20,41 @@ export default {
   name: 'Alphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
